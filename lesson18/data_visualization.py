2410665166ce886b35540bb1515d661675731e13
Leart Shaqiri <141953501+LeartShaqiri@users.noreply.github.com>
2026-01-17 14:05:06 +0100

    lesspn 18

--- lesson18/data_visualization.py
+++ lesson18/data_visualization.py
@@ -40,3 +40,71 @@ with col2:
     st.subheader("top 10 books titles")
     top_titles = books_df['author'].values_counts().head(10)
     st.bar_chart(top_authors)
+
+st.subheader("Dataset Preview")
+st.write(books_df.head())
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Top 10 Book Titles")
+    top_titles = books_df['Name'].value_counts().head(10)
+    st.bar_chart(top_titles)
+
+with col2:
+    st.subheader("Top 10 Authors")
+    top_authors = books_df['Author'].value_counts().head(10)
+    st.bar_chart(top_authors)
+
+
+
+
+
+
+st.subheader("Genre Distribution")
+fig = px.pie(
+    books_df,
+    names="Genre",
+    title="Most Liked Genre (2009–2022)",
+    color="Genre",
+    color_discrete_sequence=px.colors.sequential.Plasma
+)
+st.plotly_chart(fig)
+
+st.subheader("Number of Fiction vs Non-Fiction Books Over the Years")
+size = books_df.groupby(['Year', 'Genre']).size().reset_index(name='Counts')
+
+fig = px.bar(
+    size,
+    x="Year",
+    y="Counts",
+    color="Genre",
+    title="Number of Fiction vs Non-Fiction Books from 2009–2022",
+    color_discrete_sequence=px.colors.sequential.Plasma,
+    barmode="group"
+)
+st.plotly_chart(fig)
+
+
+
+st.subheader("Top 15 authors by counts of the books published (2009–2022)")
+top_authors = books_df['Author'].value_counts().head(15).reset_index()
+top_authors.columns = ['Author', 'Count']
+
+fig = px.bar(
+    top_authors,
+    x='Author',
+    y='Count',
+    title='Top 15 Authors by Number of Books Published (2009–2022)',
+    color='Count',
+    color_continuous_scale=px.colors.sequential.Plasma, labels={'Count': 'Number of Books Published' }
+
+)
+
+st.plotly_chart(fig)
+
+
+st.subheader("filter data by genre")
+genre_filter = st.selectbox("Select Genre", options=books_df['Genre'].unique())
+filtered_data = books_df[books_df['Genre'] == genre_filter]
+st.write(filtered_df)
